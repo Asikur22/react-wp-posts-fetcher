de5e75c745fbff31b8eb174efb9f79fd479fbff0
Asiqur Rahman <asikur22@gmail.com>
2025-03-25 19:04:35 +0600

    typo updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export class Header extends Component {
 	render() {
 		return (
-			<div className='py-4' style={{ backgroundColor: '#f4f4f4' }}>
-				<h1>WP Posts Feacher</h1>
+			<div className="py-4" style={{ backgroundColor: "#f4f4f4" }}>
+				<h1>WP Posts Fetcher from wp-json</h1>
 			</div>
 		);
 	}
